e36c7b353122f0bd76f4c6f5858703afe53b84c2
Daniel Rozenberg <me@danielrozenberg.com>
2020-03-29 16:44:15 -0400

    Be less smart about extracting the text and just get the text from all <p> elements

--- content_scripts/script.js
+++ content_scripts/script.js
@@ -1,17 +1,13 @@
-const BodyExtractor = require('extract-main-text');
 const readingTime = require('reading-time');
-
-const textPromise = new BodyExtractor(
-    {html: document.body.outerHTML},
-    {debug: false}).analyze();
 
 const optionsPromise = browser.storage.sync.get('wordsPerMinute');
 
-Promise.all([textPromise, optionsPromise]).then((results) => {
-  const [text, options] = results;
+optionsPromise.then((options) => {
+  const text = Array.from(document.querySelectorAll('p'))
+      .map((p) => p.textContent)
+      .join(' ');
   const readingStats = readingTime(text, options);
   browser.runtime.sendMessage(readingStats);
 });
 
-exports.textPromise = textPromise;
 exports.optionsPromise = optionsPromise;
